5391e6a23db8a41b9ac48d7a37f280f5474d6219
GiserDeveloper <864443272@qq.com>
2020-03-07 21:37:10 +0800

    新增:要素区菜单目录

--- mainwidget.cpp
+++ mainwidget.cpp
@@ -3,6 +3,8 @@
 #include <qgsvectorlayer.h>
 #include "mainwidget.h"
 
+#include "gwmodelmappanel.h"
+
 MainWidget::MainWidget(QWidget *parent)
     : QWidget(parent)
     , mainLayout(new QVBoxLayout)
@@ -75,6 +77,11 @@ void MainWidget::createFeaturePanel()
     QStringList headerLabels = QStringList() << tr("Features");
     mapModel->setHorizontalHeaderLabels(headerLabels);
     featurePanel->setModel(mapModel);
+    // 设置右键菜单
+    featurePanel->setContextMenuPolicy(Qt::CustomContextMenu);
+    //connect(featurePanel, &QTreeView::clicked, this, &MainWidget::ShowContextMenu);
+    // 建立信号槽的连接
+    connect(featurePanel, SIGNAL(customContextMenuRequested(const QPoint&)),this,SLOT(showContextMenu(const QPoint&)));
 }
 
 void MainWidget::createPropertyPanel()
@@ -86,3 +93,79 @@ void MainWidget::createPropertyPanel()
     propertyPanel->addTab(demoTab, tr("Property"));
     // [End] Demo Tab
 }
+
+// 官网示例(支持左键事件)
+void MainWidget::ShowContext(const QModelIndex &index)
+{
+    QStandardItem *item = mapModel->itemFromIndex(index);
+    qDebug() << index;
+}
+// 发出信号
+void MainWidget::customContextMenuRequested(const QPoint &pos)
+{
+    emit showContextMenu(pos);
+}
+// 槽函数
+void MainWidget::showContextMenu(const QPoint &pos)
+{
+    // 获取要素区列表索引值
+    QModelIndex index = featurePanel->indexAt(pos);
+    // qDebug() << index;
+    if (index.isValid())
+    {
+        QMenu *menu = new QMenu(this);
+        QAction *pShow = new QAction("显示",this);
+        menu->addAction(pShow);
+        // 处理事件
+        connect(pShow, SIGNAL(triggered()),this, SLOT(showLayer()));
+
+        // 改为"五个字的 缩放至图层"会报错, 原因未知
+        QAction *pZoom = new QAction("缩放图层",this);
+        menu->addAction(pZoom);
+
+        QAction *pAttribute = new QAction("属性表",this);
+//        pAttribute->setCheckable(true);
+//        pAttribute->setChecked(true);
+        menu->addAction(pAttribute);
+
+        QAction *pProj = new QAction("投影到坐标系",this);
+        menu->addAction(pProj);
+
+        QAction *pSymbol = new QAction("符号",this);
+        menu->addAction(pSymbol);
+        // 导出是二级菜单
+        QAction *pExport = new QAction("导出",this);
+        // menu->addAction("导出");
+        // 二级菜单制作
+        QMenu *subMenu = new QMenu(this);
+        QAction *pESRI = new QAction("ESRI Shapefile",subMenu);
+        subMenu->addAction(pESRI);
+
+        QAction *pGeo = new QAction("GeoJSON",subMenu);
+        subMenu->addAction(pGeo);
+
+        QAction *pCsv = new QAction("csv",subMenu);
+        subMenu->addAction(pGeo);
+
+        QAction *pXls = new QAction("Excel",subMenu);
+        subMenu->addAction(pXls);
+        // 设置二级菜单
+        pExport->setMenu(subMenu);
+        menu->addMenu(subMenu);
+        // QCursor::pos()让menu的位置在鼠标点击的的位置
+        menu->exec(QCursor::pos());
+    }
+}
+// 显示图层信号
+//void MainWidget::showLayerSig(const QModelIndex &index)
+//{
+//    emit showLayer(index);
+//}
+// 显示图层
+void MainWidget::showLayer()
+{
+    QModelIndexList selected = featurePanel->selectionModel()->selectedIndexes();
+    qDebug() << selected[0];
+}
+
+
--- mainwidget.h
+++ mainwidget.h
@@ -27,16 +27,26 @@ public:
     QTabWidget* propertyPanel;
 
     QStandardItemModel* mapModel;
-
 public slots:
     void openFileImportShapefile();
     void openFileImportJson();
     void openFileImportCsv();
-
+    // 要素区右键显示菜单
+    void showContextMenu(const QPoint &pos);
+    // 显示图层
+    void showLayer();
 private:
     void createMainZone();
     void createFeaturePanel();
     void createPropertyPanel();
+    // 官网示例函数
+    void ShowContext(const QModelIndex &index);
+    // 要素区右键显示菜单信号函数
+    void customContextMenuRequested(const QPoint &pos);
+    // 切换图层信号函数
+    void showLayerSig(const QModelIndex &index);
+    // 发送信号
+    void sendDataSig(const QModelIndex &index);
 };
 
 #endif // MAINLAYOUT_H
