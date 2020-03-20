e14e50460fdf00ddeb61a99a67bc8c2baf7ce6a3
Yigong Hu <huyg0180110559@outlook.com>
2020-03-20 15:46:16 +0800

    修改：工具条组件换为 qml 布局

--- gwmtoolbar.cpp
+++ gwmtoolbar.cpp
@@ -1,87 +1,27 @@
 #include "gwmtoolbar.h"
+#include "ui_gwmtoolbar.h"
 
 GwmToolbar::GwmToolbar(QWidget *parent) :
-    QWidget(parent)
+    QWidget(parent),
+    ui(new Ui::GwmToolbar)
 {
-    this->setupUi();
+    ui->setupUi(this);
+    connect(ui->openLayerBtn, &QPushButton::clicked, this, &GwmToolbar::openFileImportShapefileSignal);
+    connect(ui->saveLayerBtn, &QPushButton::clicked, this, &GwmToolbar::openFileImportJsonSignal);
+    connect(ui->saveAsBtn, &QPushButton::clicked, this, &GwmToolbar::openFileImportCsvSignal);
+    connect(ui->openByXYBtn, &QPushButton::clicked, this, &GwmToolbar::openByXYBtnSingnal);
+    connect(ui->selectBtn,  &QPushButton::clicked,  this , &GwmToolbar::selectBtnSignal);
+    connect(ui->moveBtn, &QPushButton::clicked, this, &GwmToolbar::moveBtnSignal);
+    connect(ui->editBtn, &QPushButton::clicked, this, &GwmToolbar::editBtnSignal);
+    connect(ui->zoomFullBtn, &QPushButton::clicked, this, &GwmToolbar::zoomFullBtnSignal);
+    connect(ui->zoomToLayerBtn, &QPushButton::clicked, this, &GwmToolbar::zoomToLayerBtnSignal);
+    connect(ui->zoomToSelectionBtn, &QPushButton::clicked, this, &GwmToolbar::zoomToSelectionBtnSignal);
+    connect(ui->gwmodelGWRBtn, &QPushButton::clicked, this, &GwmToolbar::gwmodelGWRBtnSignal);
+    connect(ui->gwmodelGWSSBtn, &QPushButton::clicked, this, &GwmToolbar::gwmodelGWSSBtnSignal);
+    connect(ui->gwmodelGWPCABtn, &QPushButton::clicked, this, &GwmToolbar::gwmodelGWPCABtnSignal);
 }
 
 GwmToolbar::~GwmToolbar()
 {
-
-}
-
-void GwmToolbar::setupUi()
-{
-    createButtons();
-    QHBoxLayout* widgetLayout = new QHBoxLayout(this);
-    widgetLayout->setMargin(0);
-    widgetLayout->setSpacing(0);
-    widgetLayout->addWidget(openLayerBtn);
-    widgetLayout->addWidget(openByXYBtn);
-    widgetLayout->addWidget(saveLayerBtn);
-    widgetLayout->addWidget(exportLayerBtn);
-    QFrame* seperator1 =  new QFrame(this);
-    seperator1->setLineWidth(1);
-    seperator1->setFrameStyle(QFrame::Shape::VLine);
-    widgetLayout->addWidget(seperator1);
-    widgetLayout->addWidget(selectBtn);
-    widgetLayout->addWidget(moveBtn);
-    widgetLayout->addWidget(editBtn);
-    widgetLayout->addWidget(zoomFull);
-    widgetLayout->addWidget(zoomToLayer);
-    widgetLayout->addWidget(zoomToSelection);
-    QFrame* seperator2 =  new QFrame(this);
-    seperator2->setLineWidth(1);
-    seperator2->setFrameStyle(QFrame::Shape::VLine);
-    widgetLayout->addWidget(seperator2);
-    widgetLayout->addWidget(gwmodelGWRBtn);
-    widgetLayout->addWidget(gwmodelGWSSBtn);
-    widgetLayout->addWidget(gwmodelGWPCABtn);
-    widgetLayout->addStretch();
-    this->setLayout(widgetLayout);
-    widgetLayout->setSpacing(5);
-    connect(openLayerBtn,&QPushButton::clicked,this,&GwmToolbar::openFileImportShapefileSignal);
-    connect(saveLayerBtn,&QPushButton::clicked,this,&GwmToolbar::openFileImportJsonSignal);
-    connect(exportLayerBtn,&QPushButton::clicked,this,&GwmToolbar::openFileImportCsvSignal);
-    connect(openByXYBtn,&QPushButton::clicked,this,&GwmToolbar::openByXYBtnSingnal);
-    connect(selectBtn, &QPushButton::clicked, this, &GwmToolbar::selectBtnSignal);
-    connect(moveBtn,&QPushButton::clicked,this,&GwmToolbar::moveBtnSignal);
-    connect(editBtn,&QPushButton::clicked,this,&GwmToolbar::editBtnSignal);
-    connect(zoomFull,&QPushButton::clicked,this,&GwmToolbar::zoomFullBtnSignal);
-    connect(zoomToLayer,&QPushButton::clicked,this,&GwmToolbar::zoomToLayerBtnSignal);
-    connect(zoomToSelection,&QPushButton::clicked,this,&GwmToolbar::zoomToSelectionBtnSignal);
-    connect(gwmodelGWRBtn,&QPushButton::clicked,this,&GwmToolbar::gwmodelGWRBtnSignal);
-    connect(gwmodelGWSSBtn,&QPushButton::clicked,this,&GwmToolbar::gwmodelGWSSBtnSignal);
-    connect(gwmodelGWPCABtn,&QPushButton::clicked,this,&GwmToolbar::gwmodelGWPCABtnSignal);
-}
-
-QPushButton* createToolbarButton(QString icon, QString tooltip)
-{
-    QPushButton* button = new QPushButton();
-    button->setIcon(QPixmap(icon));
-    button->setIconSize(QSize(24, 24));
-    button->setFlat(true);
-    button->setToolTip(tooltip);
-    return button;
-}
-
-void GwmToolbar::createButtons()
-{
-    openLayerBtn = createToolbarButton(QStringLiteral(":/images/themes/default/mActionAddOgrLayer.svg"), tr("Open"));
-    openByXYBtn = createToolbarButton(QStringLiteral(":/images/themes/default/mActionAddDelimitedTextLayer.svg"), tr("Open By XY Coordinate"));
-    saveLayerBtn = createToolbarButton(QStringLiteral(":/images/themes/default/mActionFileSave.svg"), tr("Save"));
-    exportLayerBtn = createToolbarButton(QStringLiteral(":/images/themes/default/mActionFileSaveAs.svg"), tr("Export"));
-    editBtn = createToolbarButton(QStringLiteral(":/images/themes/default/mActionToggleEditing.svg"), tr("Edit"));
-    selectBtn = createToolbarButton(QStringLiteral(":/images/themes/default/mActionIdentify.svg"), tr("Select"));
-    moveBtn = createToolbarButton(QStringLiteral(":/images/themes/default/mActionPan.svg"), tr("Move"));
-    zoomFull = createToolbarButton(QStringLiteral(":/images/themes/default/mActionZoomFullExtent.svg"), tr("Zoom Full"));
-    zoomToLayer = createToolbarButton(QStringLiteral(":/images/themes/default/mActionZoomToLayer.svg"), tr("Zoom to Layer"));
-    zoomToSelection = createToolbarButton(QStringLiteral(":/images/themes/default/mActionZoomToArea.svg"), tr("zoom to Selection"));
-
-    gwmodelGWRBtn = new QPushButton(tr("GWR"));
-
-    gwmodelGWSSBtn = new QPushButton(tr("GWSS"));
-
-    gwmodelGWPCABtn = new QPushButton(tr("GWPCA"));
+    delete ui;
 }
