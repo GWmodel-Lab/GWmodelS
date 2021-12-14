a753c2c203b1e672f49d9c25e8aa6708e5a08761
Enjoy <yinzuoyao@whu.edu.cn>
2021-12-14 20:12:07 +0800

    借用该分支提交Infomation界面分辨率适配BUG

--- aboutdevelopteam.cpp
+++ aboutdevelopteam.cpp
@@ -8,6 +8,8 @@ aboutdevelopteam::aboutdevelopteam(QWidget *parent) :
     ui->setupUi(this);
     ui->label_6->setText(tr("<a href = 'http://gwmodel.whu.edu.cn/'>http://gwmodel.whu.edu.cn/</a>"));
     ui->label_6->setOpenExternalLinks( true );
+    setWindowFlags(windowFlags()&~Qt::WindowMaximizeButtonHint);    // 禁止最大化按钮
+    setFixedSize(this->width(),this->height());                     // 禁止拖动窗口大小
 }
 
 aboutdevelopteam::~aboutdevelopteam()
