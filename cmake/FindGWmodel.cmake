d84ff386d55b7500625633dc0e8c3030ae6233dd
HPDell <huyg0180110559@outlook.com>
2023-07-08 14:45:03 +0100

    edit: update find gwmodel

--- cmake/FindGWmodel.cmake
+++ cmake/FindGWmodel.cmake
@@ -8,56 +8,57 @@
 #
 # The following variables will be defined:
 #
-# ``GWMODEL_FOUND`` True if GWmodel found on the local system
+# ``GWmodel_FOUND`` True if GWmodel found on the local system
 #
-# ``GWMODEL_INCLUDE_DIR`` Location of GWmodel header files
+# ``GWmodel_INCLUDE_DIR`` Location of GWmodel header files
 #
-# ``GWMODEL_LIBRARY_DIR`` Location of GWmodel libraries
+# ``GWmodel_LIBRARY_DIR`` Location of GWmodel libraries
 #
-# ``GWMODEL_LIBRARIES`` List of the GWmodel libraries found
+# ``GWmodel_LIBRARIES`` List of the GWmodel libraries found
 #
 
-find_file(GWMODEL_INCLUDE_FILE
-    NAMES gwmodel.h
-    PATHS 
-    /usr/include
-    /usr/include/gwmodel
-    /usr/local/include
-    /usr/local/include/gwmodel
-    /usr/local/gwmodel/include
-    "$ENV{LIB_DIR}/include"
-    "$ENV{INCLUDE}"
-)
-
-find_library(GWMODEL_LIBRARIES
-    NAMES gwmodel.lib libgwmodel.a
-    PATHS
-    /usr/lib
-    /usr/local/lib
-    /usr/local/gwmodel/lib
-    "$ENV{LIB_DIR}/lib"
-    "$ENV{LIB}/lib"
-    "$ENV{USR_LOCAL}/lib"
-)
-
-if(GWMODEL_INCLUDE_FILE)
-    get_filename_component(GWMODEL_INCLUDE_DIR ${GWMODEL_INCLUDE_FILE} DIRECTORY CACHE)
-    set(GWMODEL_INCLUDE_FOUDN TRUE)
-endif(GWMODEL_INCLUDE_FILE)
-
-
-if(GWMODEL_LIBRARIES)
-    set(GWMODEL_LIBRARIES_FOUND TRUE)
-endif()
-
-if(GWMODEL_INCLUDE_FOUDN AND GWMODEL_LIBRARIES_FOUND)
-    set(GWMODEL_FOUND TRUE)
-endif()
-
-if(GWMODEL_FOUND)
-    message(STATUS "Found GWmodel: ${GWMODEL_LIBRARIES}")
+if(DEFINED ENV{USR} AND NOT "$ENV{USR}" EQUAL "")
+    set(USR_DIR "$ENV{USR}")
 else()
-    if(GWMODEL_FIND_REQUIRED)
+    set(USR_DIR "/usr")
+endif()
+
+if(DEFINED ENV{USR_LOCAL} AND NOT "$ENV{USR_LOCAL}" EQUAL "")
+    set(USR_LOCAL_DIR "$ENV{USR_LOCAL}")
+else()
+    set(USR_LOCAL_DIR "/usr/local")
+endif()
+
+
+set(GWmodel_SEARCH_PATHS
+    "${USR_DIR}"
+    "${USR_LOCAL_DIR}"
+    "${CMAKE_PREFIX_PATH}"
+    "$ENV{LIB_DIR}"
+    "$ENV{LIB}"
+)
+
+find_path(GWmodel_INCLUDE_DIRS
+    NAMES gwmodel.h
+    PATHS ${GWmodel_SEARCH_PATHS}
+    PATH_SUFFIXES "include"
+)
+
+
+find_library(GWmodel_LIBRARIES
+    NAMES gwmodel.lib libgwmodel.a libgwmodel.lib
+    PATHS ${GWmodel_SEARCH_PATHS}
+    PATH_SUFFIXES "lib"
+)
+
+if(GWmodel_INCLUDE_DIRS AND GWmodel_LIBRARIES)
+    set(GWmodel_FOUND TRUE CACHE BOOL "Whether GWmodel is Found")
+endif()
+
+if(GWmodel_FOUND)
+    message(STATUS "Found GWmodel: ${GWmodel_LIBRARIES}")
+else()
+    if(GWmodel_FIND_REQUIRED)
         message(FATAL_ERROR "Could not find GWmodel")
-    endif(GWMODEL_FIND_REQUIRED)
+    endif(GWmodel_FIND_REQUIRED)
 endif()
