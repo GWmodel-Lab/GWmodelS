68d5d9a619b9121a2ea12879de6632bfc7454f96
Nianshuo Wen <1565408369@qq.com>
2025-11-13 11:57:10 +0800

    edit: set class gwda taskthread

---
+++ src/TaskThread/gwmgwdataskthread.cpp
@@ -0,0 +1,3 @@
+﻿#include "gwmgwdataskthread.h"
+
+GwmGWDATaskThread::GwmGWDATaskThread() {}
---
+++ src/TaskThread/gwmgwdataskthread.h
@@ -0,0 +1,23 @@
+﻿#ifndef GWMGWDATASKTHREAD_H
+#define GWMGWDATASKTHREAD_H
+
+#include <QObject>
+#include <armadillo>
+#include <gwmodel.h>
+
+#include "TaskThread/gwmspatialmonoscalealgorithm.h"
+#include "TaskThread/imultivariableanalysis.h"
+#include "TaskThread/iparallelable.h"
+#include "TaskThread/gwmbandwidthsizeselector.h"
+
+#include "Model/gwmalgorithmmetavariable.h"
+
+class GwmGWDATaskThread: public GwmSpatialMonoscaleAlgorithm, public IGwmMultivariableAnalysis, public IOpenmpParallelable
+{
+
+public:
+    GwmGWDATaskThread();
+
+};
+
+#endif // GWMGWDATASKTHREAD_H
